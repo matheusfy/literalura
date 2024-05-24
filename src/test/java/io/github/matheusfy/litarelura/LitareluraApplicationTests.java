df3b7815811ab840abca1bf984f88d411deb1caa
matheusfy <matheusy@alunos.utfpr.edu.br>
2024-05-24 10:54:28 -0300

    Adding initial commit to Repo

---
+++ src/test/java/io/github/matheusfy/litarelura/LitareluraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheusfy.litarelura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitareluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
